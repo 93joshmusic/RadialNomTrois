16b9bdf2d8e6a0b5cc48f05c0d2b48055f446779
93joshmusic <0parsj20@solent.ac.uk>
2016-05-17 18:49:02 +0100

    all

---
+++ ShootEmUpPardner/Assets/brawlerAttackScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brawlerAttackScript : MonoBehaviour {
+
+	public GameObject player;
+
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Destroy (gameObject, 1);
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+
+
+			Vector3 dir = transform.position - player.transform.position;
+
+			print (dir);
+
+			player.GetComponent<Rigidbody2D>().AddForce(-dir.normalized*force*Time.deltaTime);
+
+		}
+	}
+
+
+}
